140d0e894020a5e9895f7390ccb10e4985a31b29
Alvin Null <supdrewin@outlook.com>
2025-09-03 02:39:58 -0400

    fix

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@ use anyhow::*;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut writer = csv::Writer::from_path("baidu_pan_records.csv")?;
+    let mut csv = csv::Writer::from_path("baidu_pan_records.csv")?;
     let body = reqwest::get("https://www.qingju.org").await?.text().await?;
-    let mut posts = vec![find_posts(&body)?];
     let dom = tl::parse(&body, Default::default())?;
     let parser = dom.parser();
+
+    csv.write_record(&["Title", "Mirror 1", "Mirror 2", "Mirror 3", "Mirror 4"])?;
 
     if let Some(last) = dom
         .query_selector("a.page-numbers")
@@ -15,6 +16,10 @@ async fn main() -> Result<()> {
         .filter_map(|node| node.inner_text(parser).parse::<usize>().ok())
         .last()
     {
+        let mut posts = Vec::with_capacity(last);
+
+        posts.push(find_posts(&body)?);
+
         for i in 2..=last {
             let url = format!("https://www.qingju.org/page/{i}");
             let body = reqwest::get(url).await?.text().await?;
@@ -23,25 +28,31 @@ async fn main() -> Result<()> {
         }
 
         for (title, url) in posts.into_iter().flat_map(|posts| posts.into_iter()) {
-            let mut record = vec![title];
+            let mut record = vec![Default::default(); 5];
             let body = reqwest::get(url).await?.text().await?;
             let dom = tl::parse(&body, Default::default())?;
             let parser = dom.parser();
 
-            record.extend(
-                dom.query_selector("a[href]")
-                    .context(line!())?
-                    .filter_map(|handle| handle.get(parser))
-                    .filter_map(|node| node.as_tag())
-                    .filter_map(|tag| tag.attributes().get("href").flatten())
-                    .map(|url| url.as_utf8_str().to_string())
-                    .filter(|url| url.starts_with("https://pan.baidu.com")),
-            );
-            writer.write_record(record)?;
+            record[0] = title;
+
+            for (mirror, i) in dom
+                .query_selector("a[href]")
+                .context(line!())?
+                .filter_map(|handle| handle.get(parser))
+                .filter_map(|node| node.as_tag())
+                .filter_map(|tag| tag.attributes().get("href").flatten())
+                .map(|url| url.as_utf8_str().to_string())
+                .filter(|url| url.starts_with("https://pan.baidu.com"))
+                .zip(1..5)
+            {
+                record[i] = mirror;
+            }
+
+            csv.write_record(record)?;
         }
     }
 
-    Ok(writer.flush()?)
+    Ok(csv.flush()?)
 }
 
 fn find_posts<'a>(body: &str) -> Result<Vec<(String, String)>> {
